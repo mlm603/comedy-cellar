31ad2add17e9a1053129fc2eeacba41d3cd44cd2
mlm603 <meghan.maloy@datadoghq.com>
2019-06-08 18:39:41 -0400

    adjusting by screen size

--- site/static/js/custom.js
+++ site/static/js/custom.js
@@ -1,37 +1,76 @@
-d3.select("body")
-  .append("div")
-
 const w = 500;
 const h = 300;
 const padding = 20;
 
-const svg = d3.select("main")
+function make_top_comedians_chart(){
+
+  const svg = d3.select("#top_comedians")
               .append("svg")
-              .attr("width", w)
-              .attr("height", h);
+              // .attr("width", w)
+              // .attr("height", h)
+              .attr('viewBox', '0 0 ' + w + ' ' + h);
 
-const xScale = d3.scaleLinear()
-                 .domain([0, d3.max(frequent_comedians, (d) => d.show_count)])
-                 .range([h-padding/2,padding/2]);
+  const xScale = d3.scaleLinear()
+                   .domain([0, d3.max(frequent_comedians, (d) => d.show_count)])
+                   .range([h-padding/2,padding/2]);
 
-svg.selectAll("rect")
-    .data(frequent_comedians)
-    .enter()
-    .append("rect")
-    .attr("x", 0)
-    .attr("y", (d, i) => (padding/2) + i * ((h - padding)/10))
-    .attr("height", (h - padding * 2)/10)
-    .attr("width", (d) => w - xScale(d.show_count) - padding/2)
-    .attr("class","bar")
+  svg.selectAll("rect")
+      .data(frequent_comedians)
+      .enter()
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", (d, i) => (padding/2) + i * ((h - padding)/10))
+      .attr("height", (h - padding * 2)/10)
+      .attr("width", (d) => w - xScale(d.show_count) - padding/2)
+      .attr("class","bar")
 
-svg.selectAll("text")
-    .data(frequent_comedians)
-    .enter()
-    .append("text")
-    .text((d) => (d.comedian_name + " - " + d.show_count))
-    .attr("x", padding/2)
-    .attr("y", (d, i) => (padding/2) + i * ((h - padding)/10) + 17)
-    .attr("class", "bar_label")
+  svg.selectAll("text")
+      .data(frequent_comedians)
+      .enter()
+      .append("text")
+      .text((d) => (d.comedian_name + " - " + d.show_count))
+      .attr("x", padding/2)
+      .attr("y", (d, i) => (padding/2) + i * ((h - padding)/10) + 17)
+      .attr("class", "bar_label")
+
+}
+
+function make_performance_times_chart(){
+
+  const svg = d3.select("#performance_times")
+              .append("svg")
+              // .attr("width", w)
+              // .attr("height", h)
+              .attr('viewBox', '0 0 ' + w + ' ' + h);
+
+  const xScale = d3.scaleLinear()
+                   .domain([0, d3.max(frequent_comedians, (d) => d.show_count)])
+                   .range([h-padding/2,padding/2]);
+
+  svg.selectAll("rect")
+      .data(frequent_comedians)
+      .enter()
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", (d, i) => (padding/2) + i * ((h - padding)/10))
+      .attr("height", (h - padding * 2)/10)
+      .attr("width", (d) => w - xScale(d.show_count) - padding/2)
+      .attr("class","bar")
+
+  svg.selectAll("text")
+      .data(frequent_comedians)
+      .enter()
+      .append("text")
+      .text((d) => (d.comedian_name + " - " + d.show_count))
+      .attr("x", padding/2)
+      .attr("y", (d, i) => (padding/2) + i * ((h - padding)/10) + 17)
+      .attr("class", "bar_label")
+
+}
+
+make_top_comedians_chart()
+
+make_performance_times_chart()
 
 var comedian_names = unique_comedians.map(a => a.comedian_name)
 
